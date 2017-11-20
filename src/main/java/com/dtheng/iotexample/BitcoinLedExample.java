25ef1d101126be3282db4ea382fdfa7eea7dd326
Daniel Thengvall <fender5289@gmail.com>
2017-11-20 13:35:52 -0800

    setup digital input

--- src/main/java/com/dtheng/iotexample/BitcoinLedExample.java
+++ src/main/java/com/dtheng/iotexample/BitcoinLedExample.java
@@ -1,9 +1,8 @@
 package com.dtheng.iotexample;
 
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPinDigitalOutput;
-import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 import lombok.extern.slf4j.Slf4j;
 import retrofit.RestAdapter;
 import rx.Observable;
@@ -32,11 +31,15 @@ public class BitcoinLedExample {
 
     private static final GpioPinDigitalOutput yellowLightOne = controller.provisionDigitalOutputPin(RaspiPin.GPIO_05);
 
+    private static final GpioPinDigitalInput button = controller.provisionDigitalInputPin(RaspiPin.GPIO_29);
+
     private static final RestAdapter retrofit = new RestAdapter.Builder()
             .setEndpoint("https://api.sendwyre.com/")
             .build();
 
     private static final WyreRatesService wyreRatesService = retrofit.create(WyreRatesService.class);
+
+    static boolean isDisplayingRate = false;
 
     public static void main(String[] args) {
 
@@ -45,52 +48,80 @@ public class BitcoinLedExample {
         // Reset lights
         reset();
 
-        // Creates a new stream every 15 minutes
-        Observable.interval(0, 15, TimeUnit.MINUTES, Schedulers.trampoline())
+        button.addListener(new GpioPinListenerDigital() {
+            @Override
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent gpioPinDigitalStateChangeEvent) {
+                switch (gpioPinDigitalStateChangeEvent.getState()) {
+                    case HIGH:
+                        displayExchangeRate()
+                            .subscribe(Void -> {},
+                                throwable -> log.error("subscribe error: "+ throwable.toString()));
+                }
+            }
+        });
 
-                // Turn on yellow light, indicating network request
-                .doOnNext(Void -> yellowLightOne.setState(true))
+        // Creates a new stream every 10 minutes
+        Observable.interval(0, 10, TimeUnit.MINUTES, Schedulers.trampoline())
 
-                // Get rates from api
-                .flatMap(Void -> wyreRatesService.rates())
+                .flatMap(Void -> displayExchangeRate())
 
-                // Disable yellow light
-                .doOnNext(Void -> yellowLightOne.setState(false))
-
-                // Get the USD to BTC exchange rate
-                .map(node -> Optional.ofNullable(node.get("USDBTC")))
-
-                // Make sure the exchange rate is present
-                .filter(Optional::isPresent)
-
-                // Unwrap the value
-                .map(Optional::get)
-
-                .flatMap(rate -> buildExchangeRateArray(rate)
-                        .toList())
-
-                .flatMap(exchangeRateArray -> {
-
-                    log.info("USDBTC {}", exchangeRateArray);
-
-                    // List of observables configured to trigger a light to blink a certain number of times
-                    List<Observable<Void>> oBlinks = new ArrayList<>();
-
-                    oBlinks.add(blink(redLightOne).repeat(exchangeRateArray.get(0)).ignoreElements().cast(Void.class));
-                    oBlinks.add(blink(redLightTwo).repeat(exchangeRateArray.get(1)).ignoreElements().cast(Void.class));
-                    oBlinks.add(blink(redLightThree).repeat(exchangeRateArray.get(2)).ignoreElements().cast(Void.class));
-                    oBlinks.add(blink(redLightFour).repeat(exchangeRateArray.get(3)).ignoreElements().cast(Void.class));
-                    oBlinks.add(blink(redLightFive).repeat(exchangeRateArray.get(4)).ignoreElements().cast(Void.class));
-
-                    // Run observables in order one after the other
-                    return Observable.concat(oBlinks);
-                })
                 .subscribe(Void -> {},
                         throwable -> log.error("subscribe error: "+ throwable.toString()),
                         () -> {
                             log.info("subscribe onComplete");
                             controller.shutdown();
                         });
+    }
+
+    private static Observable<Void> displayExchangeRate() {
+        return Observable.defer(() -> {
+            log.debug("displayExchangeRate isDisplayingRate {}", isDisplayingRate);
+            if (isDisplayingRate)
+                return Observable.empty();
+
+            isDisplayingRate = true;
+
+            // Turn on yellow light, indicating network request
+            yellowLightOne.setState(true);
+
+            // Get rates from api
+            return wyreRatesService.rates()
+
+                    // Disable yellow light
+                    .doOnNext(Void -> yellowLightOne.setState(false))
+
+                    // Get the USD to BTC exchange rate
+                    .map(node -> Optional.ofNullable(node.get("USDBTC")))
+
+                    // Make sure the exchange rate is present
+                    .filter(Optional::isPresent)
+
+                    // Unwrap the value
+                    .map(Optional::get)
+
+                    .flatMap(rate -> buildExchangeRateArray(rate)
+                            .toList())
+
+                    .flatMap(exchangeRateArray -> {
+
+                        log.info("USDBTC {}", exchangeRateArray);
+
+                        // List of observables configured to trigger a light to blink a certain number of times
+                        List<Observable<Void>> oBlinks = new ArrayList<>();
+
+                        oBlinks.add(blink(redLightOne).repeat(exchangeRateArray.get(0)).ignoreElements().cast(Void.class));
+                        oBlinks.add(blink(redLightTwo).repeat(exchangeRateArray.get(1)).ignoreElements().cast(Void.class));
+                        oBlinks.add(blink(redLightThree).repeat(exchangeRateArray.get(2)).ignoreElements().cast(Void.class));
+                        oBlinks.add(blink(redLightFour).repeat(exchangeRateArray.get(3)).ignoreElements().cast(Void.class));
+                        oBlinks.add(blink(redLightFive).repeat(exchangeRateArray.get(4)).ignoreElements().cast(Void.class));
+
+                        // Run observables in order one after the other
+                        return Observable.concat(oBlinks);
+                    })
+                    .toList()
+                    .doOnNext(Void -> isDisplayingRate = false)
+                    .ignoreElements().cast(Void.class);
+        });
     }
 
     private static Observable<Integer> buildExchangeRateArray(Double rate) {
@@ -118,9 +149,9 @@ public class BitcoinLedExample {
     private static Observable<Void> blink(GpioPinDigitalOutput digitalOutput) {
         return Observable.just(null)
                 .doOnNext(Void -> digitalOutput.setState(true))
-                .delay(1, TimeUnit.SECONDS)
-                .doOnNext(Void -> digitalOutput.setState(false))
                 .delay(500, TimeUnit.MILLISECONDS)
+                .doOnNext(Void -> digitalOutput.setState(false))
+                .delay(250, TimeUnit.MILLISECONDS)
                 .ignoreElements().cast(Void.class);
     }
 
